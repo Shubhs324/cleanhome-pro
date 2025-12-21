7fef4e7b66847c94745d817c606e5ccecac995cf
MoMat <mohamadmatar@hotmail.com>
2025-12-21 22:33:48 +0100

    Fix: Minimal next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Active Turbopack explicitement (supprime l'avertissement)
-  turbopack: {},
 };
 
 export default nextConfig;
